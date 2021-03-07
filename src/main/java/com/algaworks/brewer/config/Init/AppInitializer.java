132cd668b4bae6ae5f215822ef99bacbff37fef4
fabiolee8 <fabio.faf8@gmail.com>
2021-03-07 09:53:25 -0300

    Projeto Brewer 4.3

--- src/main/java/com/algaworks/brewer/config/Init/AppInitializer.java
+++ src/main/java/com/algaworks/brewer/config/Init/AppInitializer.java
@@ -1,7 +1,9 @@
 package com.algaworks.brewer.config.Init;
 
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+import javax.servlet.Filter;
 
+import org.springframework.web.filter.CharacterEncodingFilter;
 import com.algaworks.brewer.config.WebConfig;
 
 public class AppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
@@ -20,4 +22,13 @@ public class AppInitializer extends AbstractAnnotationConfigDispatcherServletIni
 		return new String[] {"/"};
 	}	
 
+	@Override
+	protected Filter[] getServletFilters() {
+		CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+		characterEncodingFilter.setEncoding("UTF-8");
+		characterEncodingFilter.setForceEncoding(true);
+		
+		return new Filter[] { characterEncodingFilter };
+	}	
+	
 }
