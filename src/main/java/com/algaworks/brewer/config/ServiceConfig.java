4529bdc2d134c7097de5a64cf61f8f8d3233f109
fabiolee8 <fabio.faf8@gmail.com>
2021-03-17 20:08:00 -0300

    Projeto Brewer 11.1

--- src/main/java/com/algaworks/brewer/config/ServiceConfig.java
+++ src/main/java/com/algaworks/brewer/config/ServiceConfig.java
@@ -4,9 +4,14 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 import com.algaworks.brewer.service.CadastroCervejaService;
+import com.algaworks.brewer.service.CadastroEstiloService;
+import com.algaworks.brewer.service.CadastroUsuarioService;
 
 @Configuration
 @ComponentScan(basePackageClasses = CadastroCervejaService.class)
+@ComponentScan(basePackageClasses = CadastroEstiloService.class)
+@ComponentScan(basePackageClasses = CadastroUsuarioService.class)
 public class ServiceConfig {
 
 }
+
--- src/main/java/com/algaworks/brewer/model/Estilo.java
+++ src/main/java/com/algaworks/brewer/model/Estilo.java
@@ -9,6 +9,9 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.NotBlank;
 
 	@Entity
 	@Table(name = "estilo")
@@ -19,7 +22,9 @@ import javax.persistence.Table;
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long codigo;
-
+	
+	@NotBlank(message= "O nome é obrigatório")
+	@Size(min = 1, max = 15, message = "O tamanho do nome deve estar entre 1 e 15 caracteres")
 	private String nome;
 	
 	@OneToMany(mappedBy = "estilo")
@@ -32,7 +37,7 @@ import javax.persistence.Table;
 	public void setCodigo(Long codigo) {
 		this.codigo = codigo;
 	}
-
+	
 	public String getNome() {
 		return nome;
 	}
